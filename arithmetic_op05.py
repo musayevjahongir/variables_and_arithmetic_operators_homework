33ac41ee3f7b46d5e299897b490d6645bc933b49
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-19 20:30:48 +0500

    kod yozdim

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,5 +1,4 @@
-# Create a variable "a"  and assign it the value of the int 72.
-
-# Create a variable "b"  and assign it the value of the int 9.
-
-# Create a variable called "answer" and assign it the division of a by b.
+a=72
+b=9
+ansver = a/b
+print(ansver)
