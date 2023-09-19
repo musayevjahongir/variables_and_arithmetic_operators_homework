5e9c3c7639e9154ce31cea99f4aec0316e87aca9
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-19 20:43:36 +0500

    kod yozdim

--- arithmetic_op08.py
+++ arithmetic_op08.py
@@ -1,7 +1,5 @@
-# Create a variable "a"  and assign it the value of the int 3.
-
-# Create a variable "b"  and assign it the value of the int 4.
-
-# Create a variable "c"  and assign it the value of the int 2.
-
-# Create a variable called 'answer' and assign it the expression value: a*b:c
+a=3
+b=4
+c=2
+ansver = a*b/c
+print (ansver)
