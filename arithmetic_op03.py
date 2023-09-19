a2448c710a634de94c887fdee90001cf1387754c
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-19 20:24:55 +0500

    kod yozdim

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,4 @@
-# Create a variable "a"  and assign it the value of the int 6.
-
-# Create a variable "b"  and assign it the value of the int 2.
-
-# Create a variable called 'answer' and assign it the difference of a and b.
+a=6
+b=2
+ansver=a-b
+print(ansver)
