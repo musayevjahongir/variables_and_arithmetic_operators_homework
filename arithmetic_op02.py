c3034e4299ad4dc6b99b60c233d923473d59de90
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-19 20:19:31 +0500

    kod yozdim

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,5 +1,4 @@
-# Create a variable "a"  and assign it the value of the int 3.
-
-# Create a variable "b"  and assign it the value of the int 4.
-
-# Create a variable called 'answer' and assign it the sum of a and b.
+a=3
+b=4
+c=a+b
+print (c)
